4ed4807a103d2d46b15adbfed96a4bf5b6fb9439
Mickael Riess <mickaelriess@mickaels-air.localdomain>
2023-03-27 00:52:35 +0200

    [added] unit test

--- deathrollerdapp/test/Lock.js
+++ deathrollerdapp/test/Lock.js
@@ -3,10 +3,14 @@ const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 describe("DeathRoller", function () {
-  it("Should deploy and get three events", async function () {
+
+
+  //create a new wallet in a const
+
+  it("Should deploy and get three events", async function () {  
     const DeathRoller = await ethers.getContractFactory("DeathRoller");
     const deathRoller = await DeathRoller.deploy();
-    await deathRoller.deployed();
+
 
     expect(await deathRoller.getCurrentEntryFee(0)).to.equal(10000000000000000n);
     expect(await deathRoller.getCurrentEntryFee(1)).to.equal(50000000000000000n);
